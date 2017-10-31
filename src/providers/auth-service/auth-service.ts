30eb1941facb681d0cca75784b321195526269b1
Antonio Carlos <baptistaacbc@hotmail.com>
2017-10-31 19:15:39 -0200

    Resolvido problemas com Firebase + Adicionada cadastro de Fernando

--- src/providers/auth-service/auth-service.ts
+++ src/providers/auth-service/auth-service.ts
@@ -22,7 +22,7 @@ export class AuthServiceProvider {
     } else {
       return Observable.create(observer => {
         // At this point make a request to your backend to make a real check!
-        let access = (credentials.password === "pass" && credentials.email === "email");
+        let access = (credentials.password === "verdao10" && credentials.email === "btenoriosantos@gmail.com");
         this.currentUser = new User('Simon', 'saimon@devdactic.com');
         observer.next(access);
         observer.complete();
